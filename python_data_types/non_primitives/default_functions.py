6e7a763f2a9a120550ae039b6a268c341dc474be
borys.bardyshabacusinsights.com <bardyshborys@gmail.com>
2021-09-13 14:05:37 +0300

    add descriptions and examples about default python data types

--- python_data_types/non_primitives/default_functions.py
+++ python_data_types/non_primitives/default_functions.py
@@ -60,3 +60,61 @@ o = [4, 1, 2, 6]
 print('n=', o)
 o.sort()
 print('o.sort()', o)
+
+"""
+TUPLE default functions
+"""
+
+# count
+e = (1, 2, 3, 4, 1, 1, 2)
+print('e.count(1)', e.count(1))
+print('e.count(2)', e.count(2))
+# index
+h = (1, 2, 3, 4)
+print('h.index(3)', h.index(3))
+
+"""
+DICT default functions
+"""
+# clear
+k = {1: 2, 'foo': 'bar'}
+print('k=', k)
+k.clear()
+print('k=', k)
+# copy
+k = {1: 2, 'foo': 'bar'}
+d = c.copy()
+print(d)
+print('c is d', c is d)
+# fromkeys
+k = {1: 2, 'foo': 'bar'}
+print(k.fromkeys(k.keys()))
+# get
+k = {1: 2, 'foo': 'bar'}
+print('k.get(1)', k.get(1))
+print('k.get("fo")', k.get('fo'))
+# items
+k = {1: 2, 'foo': 'bar'}
+print('k.items() = ', k.items())
+# keys
+k = {1: 2, 'foo': 'bar'}
+print('k.keys() = ', k.keys())
+# pop
+k = {1: 2, 'foo': 'bar'}
+print('k.pop("foo") = ', k.pop("foo"))
+# popitem
+k = {1: 2, 'foo': 'bar'}
+print('k.popitem() = ', k.popitem())
+# setdefault
+k = {1: 2, 'foo': 'bar'}
+print('k=', k)
+k.setdefault('lol')
+print('k.setdefault() = ', k)
+# update
+k = {1: 2, 'foo': 'bar'}
+print('k=', k)
+k.update({1: 4})
+print('k.update({1: 4}) = ', k)
+# values
+k = {1: 2, 'foo': 'bar'}
+print('k.values() = ', k.values())
